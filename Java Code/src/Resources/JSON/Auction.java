9095b40aca310ad133eda0743c30f033076bc53c
cjm721 <cjmiller721@gmail.com>
2015-01-29 04:09:17 -0500

    Start work on JSON editing. Moving over to c# though.

--- Java Code/src/Resources/JSON/Auction.java
+++ Java Code/src/Resources/JSON/Auction.java
@@ -1,5 +1,17 @@
 package Resources.JSON;
 
 public class Auction {
-
+	long auc;
+	int item;
+	String owner;
+	String ownerRealm;
+	long bid;
+	long buyout;
+	int quantity;
+	String timeLeft;
+	int rand;
+	int seed;
+	int context;
+	int[] bonusLists;
+	
 }
