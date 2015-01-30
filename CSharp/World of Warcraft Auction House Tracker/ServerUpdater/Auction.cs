132b7035f2372434a90761527cd3b6d4098a768b
cjm721 <cjmiller721@gmail.com>
2015-01-30 16:10:57 -0500

    More work on getting to parse json. Figuring out Mins/Maxes

--- CSharp/World of Warcraft Auction House Tracker/ServerUpdater/Auction.cs
+++ CSharp/World of Warcraft Auction House Tracker/ServerUpdater/Auction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,19 +8,52 @@ using System.Threading.Tasks;
 namespace ServerUpdater
 {
 
+    public class Realm
+    {
+        public string name { get; set; }
+        public string slug { get; set; }
+    }
+
+    public class BonusList
+    {
+        public int bonusListId { get; set; }
+    }
+
+    public class Modifier
+    {
+        public int type { get; set; }
+        public int value { get; set; }
+    }
+
     public class Auction
     {
-        public long auc;
-        public int item;
-        public String owner;
-        public String ownerRealm;
-        public long bid;
-        public long buyout;
-        public int quantity;
-        public String timeLeft;
-        public int rand;
-        public int seed;
-        public int context;
-        public int[] bonusLists;
+        public long auc { get; set; }
+        public int item { get; set; }
+        public string owner { get; set; }
+        public string ownerRealm { get; set; }
+        public long bid { get; set; }
+        public long buyout { get; set; }
+        public int quantity { get; set; }
+        public string timeLeft { get; set; }
+        public long rand { get; set; }
+        public long seed { get; set; }
+        public long context { get; set; }
+        public List<BonusList> bonusLists { get; set; }
+        public List<Modifier> modifiers { get; set; }
+        public int? petSpeciesId { get; set; }
+        public int? petBreedId { get; set; }
+        public int? petLevel { get; set; }
+        public int? petQualityId { get; set; }
+    }
+
+    public class Auctions
+    {
+        public List<Auction> auctions { get; set; }
+    }
+
+    public class RootObject
+    {
+        public Realm realm { get; set; }
+        public Auctions auctions { get; set; }
     }
 }
--- CSharp/World of Warcraft Auction House Tracker/ServerUpdater/Program.cs
+++ CSharp/World of Warcraft Auction House Tracker/ServerUpdater/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,26 @@ namespace ServerUpdater
     {
         static void Main(string[] args)
         {
+            String data;
 
+            var webRequest = WebRequest.Create(@"http://us.battle.net/auction-data/5bc6c35600c6bc1c75581be4a68b82a4/auctions.json");
+
+            using (var response = webRequest.GetResponse())
+            using(var content = response.GetResponseStream())
+            using(var reader = new StreamReader(content)){
+                data = reader.ReadToEnd();
+            }
+            RootObject root = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(data);
+            int max = 0;
+            foreach (Auction a in root.auctions.auctions)
+            {
+                if (a.modifiers != null && a.modifiers.Count > 0)
+                    if (a.modifiers.Count > max)
+                        max = a.modifiers.Count;
+            }
+
+            Console.WriteLine(max);
+            
         }
     }
 }
