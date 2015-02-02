272f091ca3fe6ad94afd5529cee480f1a7932e88
cjm721 <cjmiller721@gmail.com>
2015-02-02 14:53:36 -0500

    Start work on Complex Merge. Players part done. Doing research about how to make entire steps more efficent.

--- SQL/Raw Merge.sql
+++ SQL/Raw Merge.sql
@@ -1,9 +1,11 @@
+-- Get New Players and Insert it
 INSERT INTO PLAYER (Name, Server_ID) 
 SELECT DISTINCT
 	owner as Name
 ,	server.Server_ID
-FROM auction_raw
+FROM auction_raw_copy2
 INNER JOIN server on server.name = ownerRealm
 LEFT JOIN player on player.Server_ID = server.Server_ID AND player.name = owner
 
 WHERE player.Player_ID is NULL;
+
