9e191d60df3101cbbe98f0d282b0962e74d9f2cc
erexsolr <erexsolr@rose-hulman.edu>
2015-02-11 17:02:23 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	CSharp/World of Warcraft Auction House Tracker/World of Warcraft Auction House Tracker/Form1.Designer.cs (reverted from commit 679d871f674628ed69a450e37bdf910f89887dea)

--- SQL/GetRecipesForItem.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE GetRecipesForItem (GetItemID int(10))
-begin
-SELECT r.Recipe_ID, r.Name, p.Name, r.Note
-FROM recipe r, recipe_output r_out, profession p
-WHERE r.Recipe_ID = r_out.Recipe_ID AND r.Profession_ID = p.Profession_ID AND r_out.Item_ID = GetItemID;
-end
-//
